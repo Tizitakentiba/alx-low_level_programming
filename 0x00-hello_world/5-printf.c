d100d3897e360be0e909df93f1e3745fb2aac390
Tizita Kentiba <tizitakentiba@gmail.com>
2022-06-05 23:38:58 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,16 +2,16 @@
 
 
 /*
- * main - prints the phrase with proper grammar,
+ * main - Entry point
  * but the outcome is a piece of art,
- * Return: 0 if exited properly, non-zero otherwise
+ * Return: Always 0 (success)
 */
 
 int main(void)
 
 {
 
-		printf("with proper grammar, but the outcome is a piece of art.\n");
+		printf("with proper grammar, but the outcome is a piece of art,\n");
 
 			return (0);
 
