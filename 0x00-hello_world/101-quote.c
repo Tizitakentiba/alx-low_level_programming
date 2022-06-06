7a2537ae78152ab3ef625ca7b88a561f45a4e39d
Tizita Kentiba <tizitakentiba@gmail.com>
2022-06-06 05:41:02 -0700

    Task 08

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+#include<stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return:Always 0 (success)
+ *
+ */
+{
+
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, ");
+
+fprintf(stderr, "2015-10-19\n");
+
+return (1);
+
+}
