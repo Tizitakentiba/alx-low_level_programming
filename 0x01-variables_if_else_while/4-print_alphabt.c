fc13814ce4c21ebd5221ef8b36dcac19e7234f3e
Tizita Kentiba <100354821+Tizitakentiba@users.noreply.github.com>
2022-06-06 18:49:30 +0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,13 +10,20 @@ int main(void)
   char ch = 'a';
   
   while (ch <= 'z')
+    
   {
+    
     if (ch != 'e' && ch != 'q')
+    
     {
+    
       putchar(ch);
     }
+    
     ch++;
   }
+  
   putchar('\n');
+  
   return (0);
 }
