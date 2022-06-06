53a7c13f3fbe531e44e71e0eb22a63b80f3ed4e7
Tizita Kentiba <tizitakentiba@gmail.com>
2022-06-06 05:26:13 -0700

    TATask 06

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,14 +8,14 @@
  */
 int main(void)
 {
-	printf("Size of a char: %d byte(s)\n")
+	printf("Size of a char: %d byte(s)\n");
 	
-	printf("Size of an int: %d byte(s)\n")
+	printf("Size of an int: %d byte(s)\n");
 
-	printf("Size of a long int: %d byte(s)\n")
+	printf("Size of a long int: %d byte(s)\n");
 
-	printf("Size of a long long int: %d byte(s)\n")
+	printf("Size of a long long int: %d byte(s)\n");
 
-	printf("Size of a float: %d byte(s)\n")
-	return (0)
+	printf("Size of a float: %d byte(s)\n");
+	return (0);
 }
